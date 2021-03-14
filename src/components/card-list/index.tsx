d5e388361d570315bbb4af263527b497e6028cf5
haku10 <isptry5689@gmail.com>
2021-03-14 23:03:28 +0900

    map表示の修正

--- src/components/card-list/index.tsx
+++ src/components/card-list/index.tsx
@@ -10,13 +10,10 @@ const CardList: React.FC<CardListProps> = ({ info }) => {
   return (
     <div>
       <h2>{info}</h2>
+      <div>カードのタイトル</div>
       {cards.map((item) => (
-        <>
-          <div>カードのタイトル</div>
           <div>{item}</div>
-        </>
       ))}
-      ;
     </div>
   );
 };
